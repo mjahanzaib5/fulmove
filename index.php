f6469d2e41953307ff5edd454473b1dcac8410e9
mjahanzaib5 <60965623+mjahanzaib5@users.noreply.github.com>
2020-02-12 15:23:14 +0500

    Add files via upload
    new fulmove website

---
+++ index.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Full Move</title>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/style.php"); ?>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
+<div class="slider-wrapper">
+  <section class="home-slider">
+    <div class="item">
+      <div class="home-banner d-flex" style="background-image:url(/assets/images/slider-home1.jpg)">
+        <div class="container align-self-center">
+          <div class="row">
+            <div class="col-lg-9 col-xl-6">
+              <div class="home-banner-content">
+                <h1> We <span>design websites</span> and <br>
+                  <span>build digital experiences</span> <br>
+                  your audience will <span>love</span>.
+                  </h1>
+                <p class="subtitle">RESPONSIVE WEB DESIGN & DEVELOPMENT</p>
+                <a class="home-banner-link hvr-shutter-out-horizontal" href="javascript:;">OUR WORK </a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="moving-arrow-wrapper">
+    <div class="container"> <a class="moving-arrow" href="javascript:;"><img  src="/assets/images/slider-moving-arrow.png" alt="*" /></a> </div>
+  </div>
+</div>
+<section class="sec-padding-large">
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-lg-8 text-center">
+        <h2>The Perfect Blend of <br class="d-none d-lg-block" />
+          Design and Technology</h2>
+        <p class="title-para pbpx-0">We are a digital agency who focus on creating sector leading websites, e-commerce solutions, web apps, digital marketing and much more. We don’t run a production line or put time limits on creativity. Backed by a deep insight into your world, our creative design and development teams have the freedom to deliver the very best digital experiences for today and tomorrow.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="services-area sec-padding-large pb-0">
+  <div class="container-fluid p-0">
+    <div class="row">
+      <div class="col text-center pbpx-50">
+        <h2 class="gradient-txt">Fulmove Services</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-xl-3">
+        <div class="service-box-main service-box-bg1 transition bg-full">
+          <h3>Strategy</h3>
+          <p>Our discovery workshops are designed to get under the hood of your brand & empathize with your audience. We'll put a winning strategy in place to achieve your objectives.</p>
+          <div class="add-icon"></div>
+          <a class="moreFull" href="javascript:;"></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <div class="service-box-main service-box-bg2 transition bg-full">
+          <h3>Web Design</h3>
+          <p>Our digital design team create unforgettable experiences that engage & convert. We bring screens to life using interaction, animation & anything else that is right for you.</p>
+          <div class="add-icon"></div>
+          <a class="moreFull" href="javascript:;"></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <div class="service-box-main service-box-bg3 transition bg-full">
+          <h3>Development</h3>
+          <p>We develop scalable websites & applications that work. Whether it is powerful content management, e-commerce or a killer app, we have the skills to turn ideas into reality.</p>
+          <div class="add-icon"></div>
+          <a class="moreFull" href="javascript:;"></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <div class="service-box-main service-box-bg4 transition bg-full">
+          <h3>SEO & Content</h3>
+          <p>Growing your digital footprint, giving your brand a voice and making sure you're found by the right people, we'll drive more traffic that lead to quality conversions.</p>
+          <div class="add-icon"></div>
+          <a class="moreFull" href="javascript:;"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/get-in-touch.php"); ?>
+<section class="sec-padding-large">
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-lg-8 text-center pbpx-20">
+        <h2>Award-Winning Web Design <br>
+          & Development</h2>
+      </div>
+    </div>
+    <div class="row case-study-space">
+      <div class="col-lg-7"> <img class="img-fluid" src="/assets/images/case-study1.jpg" alt="*" /> </div>
+      <div class="col-lg-5">
+        <div class="case-study-box">
+          <h3>Website Design &
+            Animation for
+            Access IS</h3>
+          <p>A new website design & digital animation for Access IS, the brand that lets you move.</p>
+          <a class="gradient-txt" href="javascript:;">view case study</a> </div>
+      </div>
+    </div>
+    <div class="row case-study-space">
+      <div class="col-lg-5">
+        <div class="case-study-box">
+          <h3>Branding & Website
+            Design for LDC</h3>
+          <p>The art of lighting meets the art of digital. A stunning new brand & digital presence for LDC Lights</p>
+          <a class="gradient-txt" href="javascript:;">view case study</a> </div>
+      </div>
+      <div class="col-lg-7"> <img class="img-fluid" src="/assets/images/case-study2.jpg" alt="*" /> </div>
+    </div>
+    <div class="row case-study-space">
+      <div class="col-lg-7"> <img class="img-fluid" src="/assets/images/case-study3.jpg" alt="*" /> </div>
+      <div class="col-lg-5">
+        <div class="case-study-box">
+          <h3>E-Commerce Website
+            Design for Happy
+            Doors</h3>
+          <p>Ordering replacement doors has never made you so happy. Meet the new brand & website we created for Happy Doors.</p>
+          <a class="gradient-txt" href="javascript:;">view case study</a> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="skills-area sec-padding-80 sec-margin-large mb-0 bg-full">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col text-center pbpx-30">
+        <h2 class="hd-style1">Some of Our <br class="d-none d-lg-block" />
+          Expert Digital Skills</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-strategy icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Digital Strategy</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-responsive-design icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Responsive Web Design</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-content icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Content Management</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-type icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Copywriting</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-animation icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Digital Animation</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-basket icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">E-Commerce</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-ux icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">User Experience</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-touch icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Web Interaction</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-api icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">API & Integration</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-multimedia icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Digital Marketing</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-design icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">UI Design</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+      <div class="col-md-6 col-xl-4">
+        <div class="skills-box"> <span class="icon-server icon-skill gradient-txt transition"></span>
+          <h5 class="ff-primary">Hosting & Support</h5>
+          <a class="moreFull" href="javascript:;"></a> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="map-wrapper">
+  <section class="map-area sec-padding-100 bg-full">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h3 class="map-hd">Find Us on Map</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="cta-info-wrapper">
+    <div class="container">
+      <div class="map-info-list-box">
+        <h4 class="align-self-center">We’re Closer Than <br />
+          You Think...</h4>
+        <ul class="map-info-list">
+          <li class="d-flex transition">
+            <div class="align-self-center m-auto"> <span class="icon-headphones transition"></span>
+              <h5>Call Us</h5>
+              <p>+44-800-123-4567</p>
+            </div>
+            <a class="moreFull" href="javascript:;"></a> </li>
+          <li class="d-flex transition">
+            <div class="align-self-center m-auto"> <span class="icon-envelope-o transition"></span>
+              <h5>Email Us</h5>
+              <p>care@fulmove.com</p>
+            </div>
+            <a class="moreFull" href="javascript:;"></a> </li>
+          <li class="d-flex transition">
+            <div class="align-self-center m-auto"> <span class="icon-map-pin transition"></span>
+              <p>2 The Old Farmhouse, <br />
+                Basingstoke, RG22</p>
+            </div>
+            <a class="moreFull" href="javascript:;"></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/testimonials.php"); ?>
+
+<section class="sec-padding-medium">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="blog-box-main">
+        <img class="blog-img" src="/assets/images/blog-bg1.jpg" alt="*" />
+          <h3>What we’re thinking</h3>
+          <h4>Proud to be a top B2B <br>
+            Service Provider</h4>
+          <p>In recognition of our accomplishments and success, we’ve been named one of the top digital agencies in the UK by Clutch, a B2B research authority.</p>
+          <a class="gradient-txt" href="javascript:;">read blog</a> </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="blog-box-main">
+        <img class="blog-img" src="/assets/images/blog-bg2.jpg" alt="*" />
+          <h3>What we’re thinking</h3>
+          <h4>Proud to be a top B2B <br>
+            Service Provider</h4>
+          <p>Get your website working properly and it can be a huge driver for new business and income. To do that though you need to avoid making design mistakes.</p>
+          <a class="gradient-txt" href="javascript:;">read blog</a> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/script.php"); ?>
+</body>
+</html>
